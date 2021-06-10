ec7a035c6b9b132cb08e0b5a4451b098ca13c306
hernan lombardo <lombardo.h@hotmail.com>
2021-06-10 18:34:14 -0300

    error handling and server responses

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,19 @@
+import random
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.fields.files import ImageField
+import random
 
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='imaegs/', blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0,200)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -7,6 +7,8 @@ from django.utils.http import is_safe_url
 from django.http import HttpResponse, Http404, JsonResponse
 import random
 
+
+
 from tweets.forms import TweetForm
 from tweets.models import Tweet
 
@@ -17,21 +19,29 @@ def home_view(request, *args, **kargs):
     return render(request, "pages/home.html", {})
 
 def tweet_create_view(request, *args, **kargs):
+    print("ajax", request.is_ajax())
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next")
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status = 201)
+
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
-
+    
+    if form.errors:
+        if request.is_ajax():
+            print("400")
+            return JsonResponse(form.errors, status = 400)
     return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kargs):
     qs = Tweet.objects.all()
 
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 200)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "response": tweets_list
     }
@@ -39,14 +49,16 @@ def tweet_list_view(request, *args, **kargs):
     return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kargs):
-    try: 
-        obj = Tweet.objects.get(id=tweet_id)
-    except:
-        raise Http404
-
     data = {
         "id": tweet_id,
-        "content": obj.content
     }
+    status = 200
+    try: 
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
 
-    return JsonResponse(data)
+
+    return JsonResponse(data, status=status)
